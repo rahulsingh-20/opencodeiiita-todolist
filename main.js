98cf002ad4c86036f348d6c3669ba3d1bc7f5142
Rahul Singh <rahulapril20@gmail.com>
2021-10-14 00:16:42 +0530

    changed styling, added clear all button

--- main.js
+++ main.js
@@ -1,38 +1,12 @@
-{/* <script src="https://kit.fontawesome.com/6ac46c55a3.js" q="anonymous"></script> */}
-
 let addToDoButton = document.getElementById('add-task-button');
-let toDoContainer = document.getElementById('task-container');
+let toDoContainer = document.getElementById('task-list-container');
 let inputField = document.getElementById('inputField');
+let clearAllButton = document.getElementById('clear-all');
 
 addToDoButton.addEventListener('click',function(e){
     e.preventDefault();
 
     if(inputField.value != ''){
-        // create a new paragraph element
-        //var paragraph = document.createElement('p');
-
-        // add styling to the new element
-        //paragraph.classList.add('paragraph-styling');
-
-        // change the text of the element to the input box content
-        //paragraph.innerText = inputField.value;
-
-        // add it to the div toDoContainer
-        //toDoContainer.appendChild(paragraph);
-
-        // reset value of input box after adding
-        //inputField.value = '';
-
-        // slash item on click
-        //paragraph.addEventListener('click',function(){
-            //paragraph.style.textDecoration = 'line-through';
-        //})
-        
-        // remove item on double click
-        //paragraph.addEventListener('dblclick', function(){
-            //toDoContainer.removeChild(paragraph);
-        //})
-
 
         var newTaskContainer = document.createElement("div");
         newTaskContainer.classList.add('task-slot');
@@ -46,11 +20,9 @@ addToDoButton.addEventListener('click',function(e){
 
         var newTaskDoneButton = document.createElement('button');
         newTaskDoneButton.classList.add('task-done-button');
-        // newTaskDoneButton.innerText = 'Done';
 
         var newTaskRemoveButton = document.createElement('button');
         newTaskRemoveButton.classList.add('task-remove-button');
-        // newTaskRemoveButton.innerText = 'Remove';
 
         toDoContainer.appendChild(newTaskContainer);
         newTaskContainer.appendChild(newTaskDesc);
@@ -69,6 +41,10 @@ addToDoButton.addEventListener('click',function(e){
 
         newTaskRemoveButton.addEventListener('click', function(){
             toDoContainer.removeChild(newTaskContainer);
+        })
+
+        clearAllButton.addEventListener('click', function(){
+            toDoContainer.innerHTML = '';
         })
     }
 })
